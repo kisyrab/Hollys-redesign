505ae5c8b3f1e42dc594304d3696bc057750b578
kisyrab <906ksy@naver.com>
2024-07-15 17:46:53 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,48 @@
+$(function(){
+    //메뉴
+    $(".main>li, .sub_bg").hover(function(){
+        $(".sub, .sub_bg").stop().slideDown();
+        // $(".sub").stop().show();
+        // $(".sub_bg").stop().slideDown("fast");
+    }, function(){
+        $(".sub, .sub_bg").stop().slideUp();
+        // $(".sub").stop().hide();
+        // $(".sub_bg").stop().slideUp("fast");
+
+    }) //menu hover
+
+
+    $(".move").slick({
+        autoplay:true,
+        autoplaySpeed:3000,
+        fade:true,
+        dots:true,
+        arrows:false,
+        adaptiveHeight:true,
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}) //j
